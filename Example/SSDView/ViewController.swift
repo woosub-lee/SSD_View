821e64862034b63f2bd2a8f3d12bf04b9ac8d67c
woosub-lee <woosub.dev@gmail.com>
2022-04-05 18:53:21 +0900

    #UK06 complete UIKit Example App

--- Example/SSDView/ViewController.swift
+++ Example/SSDView/ViewController.swift
@@ -13,15 +13,55 @@ class ViewController: UIViewController {
     
     @IBOutlet var sdView: SDView!
     
+    let importantPrivacyView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 22)
+        label.textAlignment = .center
+        label.text = "It is very important privacy.\n It only available for me!"
+        label.numberOfLines = 3
+        
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal, toItem: label, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: label, attribute: .trailing, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: label, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: label, attribute: .bottom, multiplier: 1, constant: 0)
+        ])
+        
+        return view
+    }()
+    
+    let screenshotWarnigView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 22)
+        label.textAlignment = .center
+        label.text = "You don't capture my Privacy!"
+        label.numberOfLines = 2
+        
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal, toItem: label, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: label, attribute: .trailing, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: label, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: label, attribute: .bottom, multiplier: 1, constant: 0)
+        ])
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let redView: UIView = {
-            let view = UIView()
-            view.backgroundColor = .red
-            return view
-        }()
         
         let blueView: UIView = {
             let view = UIView()
@@ -30,8 +70,8 @@ class ViewController: UIViewController {
         }()
         
         sdView
-            .setView(whenItIsNormal: blueView)
-            .setView(whenItIsDetected: redView)
+            .setView(whenItIsNormal: importantPrivacyView)
+            .setView(whenItIsDetected: screenshotWarnigView)
             .disAllow()
     }
 
