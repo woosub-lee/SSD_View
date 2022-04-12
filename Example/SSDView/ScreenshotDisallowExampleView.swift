9d47d71d960c665307c3400a43493ef3976867d8
woosub-lee <woosub.dev@gmail.com>
2022-04-13 00:00:31 +0900

    #SUI03 swiftUI representable test

---
+++ Example/SSDView/ScreenshotDisallowExampleView.swift
@@ -0,0 +1,21 @@
+//
+//  ScreenshotDisallowExampleView.swift
+//  SSDView_Example
+//
+//  Created by 이우섭 on 2022/04/07.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct ScreenshotDisallowExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScreenshotDisallowExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScreenshotDisallowExampleView()
+    }
+}
