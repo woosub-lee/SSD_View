a5decad1335c1a26bd992e9940d124a75862a956
woosub-lee <woosub.dev@gmail.com>
2022-04-19 23:57:28 +0900

    #SUI04 Stop implementing SwiftUI View, for now

--- Example/SSDView/ScreenshotDisallowExampleView.swift
+++ Example/SSDView/ScreenshotDisallowExampleView.swift
@@ -7,10 +7,14 @@
 //
 
 import SwiftUI
+import Pods_SSDView_Example
 
 struct ScreenshotDisallowExampleView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Update Not Yet")
+        }
     }
 }
 
--- SSDView/Classes/ScreenshotView.swift
+++ SSDView/Classes/ScreenshotView.swift
@@ -6,40 +6,25 @@
 //
 import SwiftUI
 
-public struct ScreenShotView<Content>: UIViewRepresentable where Content: View {
-    @Binding var isDisallow: Bool
+private struct ScreenShotView<Content>: UIViewRepresentable where Content: View {
     var parent: SDView
     var child: UIView
     
-    public init(disallow: Binding<Bool>, @ViewBuilder content: () -> Content) {
+    private init(@ViewBuilder content: () -> Content) {
         let uiKitHost = UIHostingController<Content>(rootView: content())
         
-        self._isDisallow = disallow
         self.parent = SDView()
         self.child = uiKitHost.view
         self.child.translatesAutoresizingMaskIntoConstraints = false
     }
     
-    public func makeUIView(context: Context) -> some UIView {
-        parent.addSubview(child)
-        NSLayoutConstraint.activate([
-            NSLayoutConstraint(item: parent, attribute: .leading, relatedBy: .equal, toItem: child, attribute: .leading, multiplier: 1, constant: 0),
-            NSLayoutConstraint(item: parent, attribute: .trailing, relatedBy: .equal, toItem: child, attribute: .trailing, multiplier: 1, constant: 0),
-            NSLayoutConstraint(item: parent, attribute: .top, relatedBy: .equal, toItem: child, attribute: .top, multiplier: 1, constant: 0),
-            NSLayoutConstraint(item: parent, attribute: .bottom, relatedBy: .equal, toItem: child, attribute: .bottom, multiplier: 1, constant: 0)
-        ])
-        
+    fileprivate func makeUIView(context: Context) -> some UIView {
+        parent.setView(whenItIsNormal: child)
         return parent
     }
-    
-    public func updateUIView(_ uiView: UIViewType, context: Context) {
-        if isDisallow {
-            parent.disAllow()
-        } else {
-            parent.allow()
-        }
-    }
-    
+    // TODO: SwiftUI Representable이 아닌 SwiftUI native로 작성할 수 있는지 확인
+    // TODO: View Memory Leak 해결
+    fileprivate func updateUIView(_ uiView: UIViewType, context: Context) { }
 }
 
 
